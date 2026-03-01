d1bc0dd1e3cb11b53b852e1e6fdd7c7ebdf35460
Apolo <apolobarbosa3891@gmail.com>
2026-03-01 10:54:26 -0300

    feat: created user, service, controller, config and repository.

---
+++ src/main/java/com/apolokll/passwordmanager/entity/User.java
@@ -0,0 +1,32 @@
+package com.apolokll.passwordmanager.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "users")
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String ROLE_USER;
+    private String ROLE_ADMIN;
+
+}
---
+++ src/main/java/com/apolokll/passwordmanager/service/UserService.java
@@ -0,0 +1,7 @@
+package com.apolokll.passwordmanager.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+}
