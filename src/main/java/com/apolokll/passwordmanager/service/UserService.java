57a6daf587df6adfe17169ee5c96943c63c82489
Apolo <apolobarbosa3891@gmail.com>
2026-03-01 11:52:30 -0300

    feat: creation of the registerUser method in the service

--- src/main/java/com/apolokll/passwordmanager/service/UserService.java
+++ src/main/java/com/apolokll/passwordmanager/service/UserService.java
@@ -1,7 +1,34 @@
 package com.apolokll.passwordmanager.service;
 
+import com.apolokll.passwordmanager.entity.User;
+import com.apolokll.passwordmanager.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+
+    public User registerUser(String email, String username, String password){
+        if (userRepository.findByEmail(email).isPresent()){
+            throw new RuntimeException("Email already registered.");
+        }
+
+        String encodePassword = passwordEncoder.encode(password); //criptografa a senha e guarda em encodePassword
+
+        User user = new User();
+        user.setUsername(username);
+        user.setEmail(email);
+        user.setPassword(encodePassword);
+
+        return userRepository.save(user);
+    }
 }
