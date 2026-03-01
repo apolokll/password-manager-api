216395912873b02524a73ff7d7a4108e9760b771
Apolo <apolobarbosa3891@gmail.com>
2026-03-01 16:29:59 -0300

    feat: creation of DTO RegisterUserRequest and implementation in the controller to register the user

--- src/main/java/com/apolokll/passwordmanager/controller/UserController.java
+++ src/main/java/com/apolokll/passwordmanager/controller/UserController.java
@@ -1,8 +1,29 @@
 package com.apolokll.passwordmanager.controller;
 
+import com.apolokll.passwordmanager.dto.RegisterUserRequest;
+import com.apolokll.passwordmanager.entity.User;
+import com.apolokll.passwordmanager.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping
 @RestController
 public class UserController {
 
+    private final UserService userService;
+
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public User userRegister(@RequestBody RegisterUserRequest request){
+        return userService.registerUser(
+                request.getEmail(),
+                request.getUsername(),
+                request.getPassword()
+        );
+    }
 }
